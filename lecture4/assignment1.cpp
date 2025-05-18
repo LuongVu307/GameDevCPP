d1a05d91996ab57c4ba3430336066c004ac34834
Gia Luong Vu <lgvu307@gmail.com>
2025-05-18 09:51:18 +0100

    Add Rectangle into assignment1
    - Create a base class CustomShape for Circle and Rectangle
    - Inheritance with polymorphism

--- lecture4/assignment1.cpp
+++ lecture4/assignment1.cpp
@@ -2,63 +2,139 @@
 #include <iostream>
 #include <vector>
 
-class Circle
+class CustomShape
 {
-    sf::CircleShape circle;
+protected:
+    sf::Font font;
+    sf::Text text;
     std::string name;
     sf::Color color;
     sf::Vector2f velocity;
-    const sf::Font& font;
-    sf::Text text;
-    float radius;
+    sf::Vector2f position;
 
-public: 
-    Circle(std::string name, float radius, sf::Vector2f position, 
-        sf::Vector2f velocity, sf::Color color, sf::Font& font) :
-        name(name), radius(radius),
+    virtual const sf::Shape& getShape() const = 0;
+
+
+public:
+    CustomShape(std::string name, sf::Vector2f position, 
+        sf::Vector2f velocity, sf::Color color, const sf::Font& font):
+        name(name),
+        position(position),
         velocity(velocity),
         color(color),
         font(font),
-        text(font, name, 18)
+        text(font, name, 14)
+        {
+        }
+
+
+    virtual const sf::Text& getText() const {return text; }
+    virtual void draw(sf::RenderWindow& window) const
     {
-        circle.setPosition(position);
-        circle.setFillColor(color);
-        circle.setOrigin({radius, radius});
-        circle.setRadius(radius);
+        window.draw(getShape());
+        window.draw(text);
+    }
+    virtual ~CustomShape() = default;
+
+    
+
+};
+
+class Circle final : public CustomShape
+{
+public: 
+    Circle(std::string name, float radius, sf::Vector2f position,
+           sf::Vector2f velocity, sf::Color color, const sf::Font& font)
+        : CustomShape(name, position, velocity, color, font), radius(radius), shape(radius)
+    {
+        shape.setOrigin({radius, radius});
+        shape.setPosition(position);
+        shape.setFillColor(color);
 
         text.setFillColor(sf::Color::White);
-
         sf::FloatRect textRect = text.getLocalBounds();
         text.setOrigin({textRect.size.x/2, textRect.size.y});
+
+        text.setPosition(position);
     }
 
     void update(float dt)
     {
-        circle.move(velocity * dt);
-        text.setPosition(circle.getPosition());
+        shape.move(velocity * dt);
+        text.setPosition(shape.getPosition());
         bool velX_sign = velocity.x > 0, velY_sign =  velocity.y > 0;
 
 
-        if ((circle.getPosition().x-radius <= 0 && velX_sign == false) ||
-            (circle.getPosition().x+radius >= 1280 && velX_sign == true)
+        if ((shape.getPosition().x-radius <= 0 && velX_sign == false) ||
+            (shape.getPosition().x+radius >= 1280 && velX_sign == true)
             )
         {
             velocity.x = -velocity.x;
         }
-        if ((circle.getPosition().y-radius <= 0 && velY_sign == false) ||
-            (circle.getPosition().y+radius >= 720 && velY_sign == true)
+        if ((shape.getPosition().y-radius <= 0 && velY_sign == false) ||
+            (shape.getPosition().y+radius >= 720 && velY_sign == true)
             )
         {
             velocity.y = -velocity.y;
         }
     }
 
-    const sf::CircleShape& getCircle() const {return circle; }
-    const sf::Text& getText() const {return text; }
+private:
+    sf::CircleShape shape;
+    float radius;
+
+    const sf::Shape& getShape() const override {return shape; }
 
 };
 
+class Rectangle final : public CustomShape
+{
+public:
+    Rectangle(std::string name, float height, float width, sf::Vector2f position,
+           sf::Vector2f velocity, sf::Color color, const sf::Font& font)
+        : CustomShape(name, position, velocity, color, font), 
+        width(width), height(height), shape({width, height})
+        {
+        shape.setOrigin({width/2, height/2});
+        shape.setPosition(position);
+        shape.setFillColor(color);
 
+        text.setFillColor(sf::Color::White);
+        sf::FloatRect textRect = text.getLocalBounds();
+        text.setOrigin({textRect.size.x/2, textRect.size.y});
+
+        text.setPosition(position);
+        }
+
+    void update(float dt)
+    {
+        shape.move(velocity * dt);
+        text.setPosition(shape.getPosition());
+        bool velX_sign = velocity.x > 0, velY_sign =  velocity.y > 0;
+
+
+        if ((shape.getPosition().x-width/2 <= 0 && velX_sign == false) ||
+            (shape.getPosition().x+width/2 >= 1280 && velX_sign == true)
+            )
+        {
+            velocity.x = -velocity.x;
+        }
+        if ((shape.getPosition().y-height/2 <= 0 && velY_sign == false) ||
+            (shape.getPosition().y+height/2 >= 720 && velY_sign == true)
+            )
+        {
+            velocity.y = -velocity.y;
+        }
+
+    }
+
+private:
+    float width, height;
+    sf::RectangleShape shape;
+
+    const sf::Shape& getShape() const override {return shape; }
+
+};
 
 int main(int argc, char * argv[])
 {
@@ -81,7 +157,11 @@ int main(int argc, char * argv[])
     Circle CGreen("CGreen", 50., {100., 100.}, {-3., 2.}, sf::Color{0, 255, 0}, font);
     Circle CBlue("CBlue", 100., {200., 200.}, {2., 4.}, sf::Color{0, 0, 255}, font);
     Circle CPurple("CPurple", 75., {300., 300.}, {-2., -1.}, sf::Color{255, 0, 255}, font);
-    
+
+    Rectangle RRed("RRed", 25, 50, {300, 300}, {4, 4}, sf::Color{255, 0, 0}, font);
+    Rectangle RGrey("RGrey", 100, 50, {300, 250}, {-2, 2}, sf::Color{100, 100, 100}, font);
+    Rectangle RTeal("RTeal", 100, 100, {25, 100}, {-2, -2}, sf::Color{255, 255, 100}, font);
+
 
     window.setFramerateLimit(FPS);
     
@@ -95,6 +175,10 @@ int main(int argc, char * argv[])
         CGreen.update(dt);
         CBlue.update(dt);
         CPurple.update(dt);
+        RRed.update(dt);
+        RGrey.update(dt);
+        RTeal.update(dt);
+
 
         while (std::optional event = window.pollEvent())
         {
@@ -106,13 +190,13 @@ int main(int argc, char * argv[])
 
         window.clear(sf::Color(0, 0, 0));
 
-        window.draw(CGreen.getCircle());
-        window.draw(CBlue.getCircle());
-        window.draw(CPurple.getCircle());
+        CGreen.draw(window);
+        CBlue.draw(window);
+        CPurple.draw(window);
+        RRed.draw(window);
+        RGrey.draw(window);
+        RTeal.draw(window);
 
-        window.draw(CGreen.getText());
-        window.draw(CBlue.getText());
-        window.draw(CPurple.getText());
 
 
         window.display();
